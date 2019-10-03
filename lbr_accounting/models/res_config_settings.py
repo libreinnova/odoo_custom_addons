52ab7a96c77a04cf1bfa2b894847ee2c2d5076b4
Paul <paul@unburst.net>
2019-10-03 11:40:03 +0200

    [ADD] LBR accounting: new optional module: l10n_es_account_bank_statement_import_n43

--- lbr_accounting/models/res_config_settings.py
+++ lbr_accounting/models/res_config_settings.py
@@ -5,5 +5,10 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     module_om_account_accountant = fields.Boolean(
-        string="Odoo12 Accounting (by Odoo Mates & Odoo SA)",
-        help="Accounting Reports, Asset Management and Account Budget For Odoo12 Community Edition")
+        string='Odoo12 Accounting (by Odoo Mates & Odoo SA)',
+        help='Accounting Reports, Asset Management and Account Budget For Odoo12 Community Edition')
+
+    module_l10n_es_account_bank_statement_import_n43 = fields.Boolean(
+        string='Import of Spanish bank statements (Rule 43)',
+        help='Click here to install this module '
+             '(by Spanish Localization Team, Tecnativa, Odoo Community Association (OCA))')

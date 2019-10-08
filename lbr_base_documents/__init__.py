7841bd3c30d7eb4f0e163363622ca61ab056d5d7
Paul <paul@unburst.net>
2019-10-08 16:15:49 +0200

    [NEW] LBR base documents module

---
+++ lbr_base_documents/__init__.py
@@ -0,0 +1,8 @@
+###############################################################################
+#
+# Copyright(c): 2019 Libreinnova (<https://libreinnova.com/>)
+# License OPL-1 - See https://www.odoo.com/documentation/user/12.0/legal/licenses/licenses.html#odoo-apps
+# See LICENSE file for full copyright and licensing details.
+# All Rights Reserved
+#
+###############################################################################
---
+++ lbr_base_documents/__manifest__.py
@@ -0,0 +1,31 @@
+###############################################################################
+#
+# Copyright(c): 2019 Libreinnova (<https://libreinnova.com/>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# All Rights Reserved
+#
+###############################################################################
+{
+    'name': 'LBR base documents',
+    'version': '1.0',
+    'summary': 'One-click install of document management system',
+    'description': '',
+    'category': 'Document Management',
+    'author': 'Libreinnova',
+    'website': 'https://libreinnova.com/',
+    'license': 'AGPL-3',
+    'images': [
+        'static/description/icon.png'
+    ],
+    'depends': [
+        'muk_dms',
+        'muk_dms_actions',
+        'muk_dms_file',
+        'muk_dms_lobject',
+        'muk_dms_mail',
+        'muk_dms_view',
+    ],
+    'data': [],
+    'installable': True,
+    'application': False,
+}

f7063f04d10ded7761f17594540575ae652adc8f
Paul <paul@unburst.net>
2019-10-16 09:28:09 +0200

    [UPD] LBR accounting: Journal Ledger menu & translation

--- lbr_accounting/__manifest__.py
+++ lbr_accounting/__manifest__.py
@@ -25,6 +25,7 @@
     ],
     'data': [
         'views/account_invoice.xml',
+        'views/account_move_line.xml',
         'views/res_config_settings.xml',
     ],
     'external_dependencies': {

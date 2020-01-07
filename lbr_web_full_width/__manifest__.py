1186990a44e44cf1effb8df99a509ccbc2779d70
Paul <paul@unburst.net>
2020-01-07 12:14:11 +0100

    [NEW] LBR web full width module

---
+++ lbr_web_full_width/__manifest__.py
@@ -0,0 +1,28 @@
+###############################################################################
+#
+# Copyright(c): 2020 Libreinnova (<https://libreinnova.com/>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# All Rights Reserved
+#
+###############################################################################
+{
+    'name': 'LBR web full width',
+    'version': '1.0',
+    'summary': 'Web form view full width',
+    'description': '',
+    'category': 'Tools',
+    'author': 'Libreinnova',
+    'website': 'https://libreinnova.com/',
+    'license': 'AGPL-3',
+    'images': [
+        'static/description/icon.png'
+    ],
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'templates/assets.xml',
+    ],
+    'installable': True,
+    'application': False,
+}

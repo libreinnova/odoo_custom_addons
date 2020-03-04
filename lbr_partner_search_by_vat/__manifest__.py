01ff05ee3884d00d000ce132af5c5c40530f2be6
Paul <paul@unburst.net>
2020-03-04 10:50:19 +0100

    [NEW] Partner search by VAT module

---
+++ lbr_partner_search_by_vat/__manifest__.py
@@ -0,0 +1,28 @@
+###############################################################################
+#
+# Copyright(c): 2020 Libreinnova (<https://libreinnova.com/>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# All Rights Reserved
+#
+###############################################################################
+{
+    'name': 'LBR partner search by VAT',
+    'version': '1.0',
+    'summary': 'Allows to search contacts by VAT code',
+    'description': '',
+    'category': 'Tools',
+    'author': 'Libreinnova',
+    'website': 'https://libreinnova.com/',
+    'license': 'AGPL-3',
+    'images': [
+        'static/description/icon.png'
+    ],
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/res_partner.xml'
+    ],
+    'installable': True,
+    'application': False,
+}

7963af2ad94ea450c41187a395bc6c77c3678bbc
Paul S <neooien@outlook.com>
2021-03-22 16:14:49 +0100

    [UPD] Accounting: dependenicies [T#2429]

--- lbr_accounting/__manifest__.py
+++ lbr_accounting/__manifest__.py
@@ -23,6 +23,7 @@
         'account_chart_update',
         'account_coa_menu',
         'account_financial_report',
+        'account_lock_date_update',
         'account_payment_partner',
         'account_banking_sepa_credit_transfer',
         'account_banking_sepa_direct_debit',

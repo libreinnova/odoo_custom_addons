af0672d802862e7c87df4608882ce8e984bd0e59
Paul S <neooien@outlook.com>
2021-10-07 11:10:33 +0200

    [ADD] No partner subscribe: account invoice exception

--- lbr_no_partner_subscribe/models/mail_thread.py
+++ lbr_no_partner_subscribe/models/mail_thread.py
@@ -1,13 +1,17 @@
 from odoo import api, fields, models
 from odoo.http import request
 
+IGNORED_MODELS = [
+    'sale.order', 'purchase.order', 'crm.lead', 'project.project', 'project.task', 'account.invoice'
+]
+
 
 class MailThread(models.AbstractModel):
     _inherit = 'mail.thread'
 
     def _message_subscribe(self, partner_ids=None, channel_ids=None, subtype_ids=None, customer_ids=None):
         # Don't add partners as a followers for next models:
-        if self._name in ['sale.order', 'purchase.order', 'crm.lead', 'project.project', 'project.task']:
+        if self._name in IGNORED_MODELS:
             if partner_ids:
                 users = request.env['res.users'].search([('partner_id', 'in', partner_ids)])
                 # Only add contact as a follower if it has a assigned Odoo user

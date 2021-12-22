79dbaf1f688d4580d4a58b1acc98931c0922dcde
Paul S <neooien@outlook.com>
2021-12-22 12:43:20 +0100

    [NEW] Redsys result [T#3336]

---
+++ lbr_redsys_result/__manifest__.py
@@ -0,0 +1,26 @@
+###############################################################################
+#
+# Copyright(c): 2021 Libreinnova (<https://libreinnova.com/>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# All Rights Reserved
+#
+###############################################################################
+{
+    'name': 'LBR Redsys result',
+    'version': '1.0',
+    'summary': 'Payment feedback on OK result',
+    'description': '',
+    'category': 'Tools',
+    'author': 'Libreinnova',
+    'website': 'https://libreinnova.com/',
+    'license': 'AGPL-3',
+    'images': [
+        'static/description/icon.png'
+    ],
+    'depends': [
+        'payment_redsys'
+    ],
+    'data': [],
+    'installable': True,
+    'application': False,
+}

e9ed5b6631c48d46bbb23ddadac0317ad56c86fb
Paul S <neooien@outlook.com>
2022-05-02 09:20:37 +0200

    [FIX] No partner subscribe: search partners by user

--- lbr_no_partner_subscribe/models/mail_thread.py
+++ lbr_no_partner_subscribe/models/mail_thread.py
@@ -1,5 +1,4 @@
 from odoo import api, fields, models
-from odoo.http import request
 
 IGNORED_MODELS = [
     'sale.order', 'purchase.order', 'crm.lead', 'project.project', 'project.task', 'account.invoice'
@@ -11,11 +10,10 @@ class MailThread(models.AbstractModel):
 
     def _message_subscribe(self, partner_ids=None, channel_ids=None, subtype_ids=None, customer_ids=None):
         # Don't add partners as a followers for next models:
-        if self._name in IGNORED_MODELS:
-            if partner_ids:
-                users = request.env['res.users'].search([('partner_id', 'in', partner_ids)])
-                # Only add contact as a follower if it has a assigned Odoo user
-                partner_ids = users.mapped('partner_id').ids if users else []
+        if (self._name in IGNORED_MODELS) and partner_ids:
+            users = self.env['res.users'].search([('partner_id', 'in', partner_ids)])
+            # Only add contact as a follower if it has an assigned Odoo user
+            partner_ids = users.mapped('partner_id').ids if users else []
         # Call to super with modified 'partner_ids' list
         return super(MailThread, self)._message_subscribe(
             partner_ids=partner_ids, channel_ids=channel_ids, subtype_ids=subtype_ids, customer_ids=customer_ids)

fc767e209b542bbdfc3f6ec09fc660250a8de357
Paul S <neooien@outlook.com>
2022-05-18 17:27:26 +0200

    [UPD] No user assigned message: return

--- lbr_no_user_assigned_message/models/mail_thread.py
+++ lbr_no_user_assigned_message/models/mail_thread.py
@@ -5,13 +5,9 @@ class MailThread(models.AbstractModel):
     _inherit = 'mail.thread'
 
     def _message_auto_subscribe_followers(self, updated_values, default_subtype_ids):
-        message = super(MailThread, self)._message_auto_subscribe_followers(updated_values, default_subtype_ids)
-        new_message = []
-        for values in message:
-            if len(values) == 3:
-                if values[2] == 'mail.message_user_assigned':
-                    continue
-            else:
-                new_message.append(values)
+        subscribe_message = super(MailThread, self)._message_auto_subscribe_followers(
+            updated_values, default_subtype_ids)
         # Return new message list without assigned notification entries
-        return new_message
+        return [
+            value for value in subscribe_message if (len(value) > 2 and value[2] != 'mail.message_user_assigned')
+        ]

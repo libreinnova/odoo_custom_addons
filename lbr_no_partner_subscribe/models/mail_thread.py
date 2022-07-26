6ac1ba0b0c874e01217be3f5c19c30e5285bba0d
Paul S <neooien@outlook.com>
2022-07-26 09:27:42 +0200

    [FIX] No partner subscribe: search request

--- lbr_no_partner_subscribe/models/mail_thread.py
+++ lbr_no_partner_subscribe/models/mail_thread.py
@@ -1,5 +1,4 @@
 from odoo import api, fields, models
-from odoo.http import request
 
 IGNORED_MODELS = [
     'sale.order', 'purchase.order', 'crm.lead', 'project.project', 'project.task', 'account.move'
@@ -13,8 +12,8 @@ class MailThread(models.AbstractModel):
         # Don't add partners as a followers for next models:
         if self._name in IGNORED_MODELS:
             if partner_ids:
-                users = request.env['res.users'].search([('partner_id', 'in', partner_ids)])
-                # Only add contact as a follower if it has a assigned Odoo user
+                users = self.env['res.users'].search([('partner_id', 'in', partner_ids)])
+                # Only add contact as a follower if it has an assigned Odoo user
                 partner_ids = users.mapped('partner_id').ids if users else []
         # Call to super with modified 'partner_ids' list
         return super(MailThread, self)._message_subscribe(
